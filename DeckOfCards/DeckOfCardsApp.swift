3722c6ac1a464bfa41800c01d40139ee7e338c56
suhit.patil@gmail.com <$rp@H0p@2019>
2024-08-25 15:30:01 +0530

    Initial Commit

---
+++ DeckOfCards/DeckOfCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeckOfCardsApp.swift
+//  DeckOfCards
+//
+//  Created by SUHIT PATIL on 25/08/24.
+//
+
+import SwiftUI
+
+@main
+struct DeckOfCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
